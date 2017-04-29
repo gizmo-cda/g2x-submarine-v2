8c0311a9f71081df20dfae489472a050e02c3c14
Kevin Lindsey <kevin@kevlindev.com>
2017-04-29 16:51:53 -0700

    Grab all compass readings for testing purposes

--- web/routes/imu.js
+++ web/routes/imu.js
@@ -1,10 +1,18 @@
 module.exports = function(app) {
-	let db = app.get('db');
-	let compass = db.collection['compass'];
-
 	app.get('/imu', (req, res, next) => {
-		compass.find({}).toArray((err, docs) => {
-			res.end(JSON.stringify(docs));
-		});
+		let db = app.get('db');
+		let compass = db.collection('compass');
+
+		if (compass !== undefined) {
+			compass.find({}).toArray((err, docs) => {
+				res.end(JSON.stringify(docs));
+			});
+		}
+		else {
+			res.end(JSON.stringify({
+				status: 500,
+				text: "cannot find 'compass' collection"
+			}));
+		}
 	});
 };

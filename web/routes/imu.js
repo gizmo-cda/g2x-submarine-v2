0de4473398fbcea5192036ffe2e14309503443f8
Kevin Lindsey <kevin@kevlindev.com>
2017-04-29 18:15:21 -0700

    Return last reading from all imu units

--- web/routes/imu.js
+++ web/routes/imu.js
@@ -1,22 +1,43 @@
+let async = require('async');
+
+function lastDocument(db, collectionName, callback) {
+	let collection = db.collection(collectionName);
+
+	collection.count((err, count) => {
+		collection.find({}).skip(count - 1).toArray(callback);
+	})
+}
+
 module.exports = function(app) {
 	app.get('/imu', (req, res, next) => {
 		let db = app.get('db');
+		let accelerometer = db.collection('accelerometer');
+		let gyroscope = db.collection('gyroscope');
 		let compass = db.collection('compass');
+		let temperature = db.collection('temperature');
 
-		if (compass !== undefined) {
-			compass
-				.find({})
-				.skip(compass.count() - 1)
-				.limit(1)
-				.toArray((err, docs) => {
-					res.end(JSON.stringify(docs[0]));
-				});
-		}
-		else {
-			res.end(JSON.stringify({
-				status: 500,
-				text: "cannot find 'compass' collection"
-			}));
-		}
+		async.parallel({
+			"accelerometer": (done) => {
+				lastDocument(db, "accelerometer", done)
+			},
+			"gyroscope": (done) => {
+				lastDocument(db, "gyroscope", done)
+			},
+			"temperature": (done) => {
+				lastDocument(db, "temperature", done)
+			},
+			"compass": (done) => {
+				lastDocument(db, "compass", done)
+			}
+		}, (err, result) => {
+			var myResult = {
+				"accelerometer": result.accelerometer[0],
+				"gyroscope": result.gyroscope[0],
+				"temperature": result.temperature[0],
+				"compass": result.compass[0],
+			}
+
+			res.end(JSON.stringify(myResult));
+		});
 	});
 };

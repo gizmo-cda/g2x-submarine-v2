cd2cbecb10837e84292993ed8375518113e32ada
Kevin Lindsey <kevin@kevlindev.com>
2017-05-16 09:36:24 -0700

    Fix whitespace

--- scripts/obj2json.js
+++ scripts/obj2json.js
@@ -7,82 +7,82 @@ let source = fs.readFileSync(filename, { encoding: 'utf-8' });
 let lines = source.split(/\r\n?|\r/);
 
 let counts = {
-	comments: 0,
-	emptyLines: 0,
-	v: 0,
-	vt: 0,
-	vn: 0,
-	f: 0,
-	s: 0,
-	g: 0,
-	mtllib: 0,
-	usemtl: 0
+    comments: 0,
+    emptyLines: 0,
+    v: 0,
+    vt: 0,
+    vn: 0,
+    f: 0,
+    s: 0,
+    g: 0,
+    mtllib: 0,
+    usemtl: 0
 };
 
 let result = {
-	vertices: [],
-	normals: [],
-	textureCoordinates: [],
-	faces: []
+    vertices: [],
+    normals: [],
+    textureCoordinates: [],
+    faces: []
 }
 
 function toNumber(value) {
-	return value === undefined ? value : +value;
+    return value === undefined ? value : +value;
 }
 
 lines.forEach((line) => {
-	if (/^\s*#.*$/.test(line)) {
-		// ignore comments
-		counts.comments++;
-	}
-	else if (/^\s*$/.test(line)) {
-		// ignore empty lines
-		counts.emptyLines++;
-	}
-	else {
-		parts = line.trim().split(/\s+/);
-		let cmd = parts[0];
-		let args = parts.slice(1);
+    if (/^\s*#.*$/.test(line)) {
+        // ignore comments
+        counts.comments++;
+    }
+    else if (/^\s*$/.test(line)) {
+        // ignore empty lines
+        counts.emptyLines++;
+    }
+    else {
+        parts = line.trim().split(/\s+/);
+        let cmd = parts[0];
+        let args = parts.slice(1);
 
-		counts[cmd]++;
+        counts[cmd]++;
 
-		switch (cmd) {
-			case "v":
-				result.vertices.push(args.map(toNumber));
-				break;
+        switch (cmd) {
+            case "v":
+                result.vertices.push(args.map(toNumber));
+                break;
 
-			case "vn":
-				result.normals.push(args.map(toNumber));
-				break;
+            case "vn":
+                result.normals.push(args.map(toNumber));
+                break;
 
-			case "vt":
-				result.textureCoordinates.push(args.map(toNumber));
-				break;
+            case "vt":
+                result.textureCoordinates.push(args.map(toNumber));
+                break;
 
-			case "f":
-				result.faces.push(args.map((value) => {
-					return value.split("/").map(toNumber);
-				}));
-				break;
+            case "f":
+                result.faces.push(args.map((value) => {
+                    return value.split("/").map(toNumber);
+                }));
+                break;
 
-			// ignoring these for now
-			case "s":
-				break;
+            // ignoring these for now
+            case "s":
+                break;
 
-			case "g":
-				break;
+            case "g":
+                break;
 
-			case "mtllib":
-				break;
+            case "mtllib":
+                break;
 
-			case "usemtl":
-				break;
+            case "usemtl":
+                break;
 
-			default:
-				console.error("unknown command:", cmd);
-				break;
-		}
-	}
+            default:
+                console.error("unknown command:", cmd);
+                break;
+        }
+    }
 });
 
 result.filename = filename;

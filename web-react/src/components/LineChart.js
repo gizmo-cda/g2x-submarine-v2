4f65269452479fc96b77afe7cc2e17c4cf59e223
Kevin Lindsey <kevin@kevlindev.com>
2017-05-21 17:25:11 -0700

    Separate background ghosting into its own component

--- web-react/src/components/LineChart.js
+++ web-react/src/components/LineChart.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Background from './Background'
 import Label from './Label';
 
 class LineChart extends React.Component {
@@ -7,8 +8,6 @@ class LineChart extends React.Component {
         y: 10,
         width: 100,
         height: 100,
-        background_color: "rgb(255,255,255)",
-        background_opacity: 0.6,
         padding: 10,
         data: [],
         path_color: "rgb(255,128,0)",
@@ -20,8 +19,6 @@ class LineChart extends React.Component {
             y                  = this.props.y,
             width              = this.props.width,
             height             = this.props.height,
-            background_color   = this.props.background_color,
-            background_opacity = this.props.background_opacity,
             data               = this.props.data,
             path_color         = this.props.path_color,
             label_text         = this.props.label;
@@ -34,7 +31,7 @@ class LineChart extends React.Component {
 
         return (
             <svg x={x} y={y} width={width} height={height}>
-                <rect width="100%" height="100%" fill={background_color} fillOpacity={background_opacity} />
+                <Background />
                 <path d={path_string} stroke={path_color} fill="none" />
                 <Label x={width * 0.5} y={height} alignment="center">{label_text}</Label>
             </svg>

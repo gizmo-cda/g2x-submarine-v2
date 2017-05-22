4145e29198846f1627948de2fc2681d226166978
Kevin Lindsey <kevin@kevlindev.com>
2017-05-21 17:50:43 -0700

    Parameterize compass drawing to make it resizeable

--- web-react/src/components/Compass.js
+++ web-react/src/components/Compass.js
@@ -3,19 +3,29 @@ import Background from './Background'
 
 class Compass extends React.Component {
     static defaultProps = {
+        width: 100,
+        height: 100,
         color: "rgb(255, 128, 0)"
     };
 
     render() {
-        let color = this.props.color;
+        let padding     = 20,
+            width       = this.props.width,
+            height      = this.props.height,
+            half_width  = width * 0.5,
+            half_height = height * 0.5,
+            origin      = `translate(${half_width}, ${half_height})`,
+            radius      = (Math.min(width, height) - padding) * 0.5 - 1,
+            color       = this.props.color,
+            offset      = `translate(${radius - 6 - 1}, 0)`;
 
         return (
-            <svg width={100} height={100}>
+            <svg width={width} height={height}>
                 <Background />
-                <g transform="translate(50,50)">
-                    <circle r={48} stroke={color} strokeWidth={1} fill="none"/>
+                <g transform={origin}>
+                    <circle r={radius} stroke={color} strokeWidth={1} fill="none"/>
                     <g transform={this.props.direction}>
-                        <g transform="translate(41, 0)">
+                        <g transform={offset}>
                             <polygon points="6,0 0,4 0,-4" fill={color}/>
                         </g>
                     </g>

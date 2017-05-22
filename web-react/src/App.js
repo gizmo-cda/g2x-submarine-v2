6cf895710028574c9d48fb092a5d3bf93feb943c
Kevin Lindsey <kevin@kevlindev.com>
2017-05-21 17:25:53 -0700

    Render fake data for a compass too

--- web-react/src/App.js
+++ web-react/src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 import LineChart from './components/LineChart';
+import Compass from './components/Compass';
 
 class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            data: []
+            depth: [],
+            temperature: [],
+            compass: []
         };
     }
 
@@ -23,24 +26,39 @@ class App extends Component {
 
     tick() {
         var t = Date.now();
-        var data = [];
+        var depth = [];
+        var temperature = [];
+        var i, value;
 
-        for (var i = 10; i < 100; i += 10) {
-            var value = ((i + t * 0.25) % 80) + 10;
+        for (i = 10; i < 100; i += 10) {
+            value = ((i + t * 0.25) % 80) + 10;
 
-            data.push([i, value]);
+            depth.push([i, value]);
         }
 
-        this.setState({ data: data });
+        for (i = 10; i < 100; i += 10) {
+            value = ((i + t * 0.25) % 80) + 10;
+
+            temperature.push([i, value]);
+        }
+
+        this.setState({
+            depth: depth,
+            temperature: temperature,
+            compass: (t / 100) % 360.0
+        });
     }
 
     render() {
-        var data = this.state.data;
+        var depth = this.state.depth;
+        var temperature = this.state.temperature;
+        var direction = `rotate(${-this.state.compass})`;
 
         return (
             <div className="App">
-                <LineChart label="-- ft" data={data}/>
-                <LineChart label="-- °F" data={data}/>
+                <LineChart label="-- ft" data={depth}/>
+                <LineChart label="-- °F" data={temperature}/>
+                <Compass direction={direction}/>
             </div>
         );
     }

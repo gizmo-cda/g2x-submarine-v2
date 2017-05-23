b70b175e5f34d232a94cef2043304f39fb2ec998
Kevin Lindsey <kevin@kevlindev.com>
2017-05-23 13:26:00 -0700

    Update sample data used in line charts

--- web-react/src/App.js
+++ web-react/src/App.js
@@ -3,6 +3,26 @@ import './App.css';
 import LineChart from './components/LineChart';
 import Compass from './components/Compass';
 
+function generateData(frequency, trigFunction) {
+    var t = Date.now();
+    var data = [];
+
+    for (var i = 0; i <= 60000; i += 1000) {
+        var x = (i / 60000) * 100;
+
+        var currentMillis = t + i;
+        var currentSeconds = currentMillis / 1000;
+        var angle = 2.0 * Math.PI * frequency * currentSeconds;
+        var value = trigFunction(angle) * 20 + 45;
+
+        value = Math.round(1000 * value) / 1000;
+
+        data.push([x, value]);
+    }
+
+    return data;
+}
+
 class App extends Component {
     constructor(props) {
         super(props);
@@ -25,27 +45,10 @@ class App extends Component {
     }
 
     tick() {
-        var t = Date.now();
-        var depth = [];
-        var temperature = [];
-        var i, value;
-
-        for (i = 10; i < 100; i += 10) {
-            value = ((i + t * 0.25) % 80) + 10;
-
-            depth.push([i, value]);
-        }
-
-        for (i = 10; i < 100; i += 10) {
-            value = ((i + t * 0.25) % 80) + 10;
-
-            temperature.push([i, value]);
-        }
-
         this.setState({
-            depth: depth,
-            temperature: temperature,
-            compass: (t / 100) % 360.0
+            depth: generateData(0.025, Math.sin),
+            temperature: generateData(0.05, Math.cos),
+            compass: (10 * Date.now() / 1000) % 360.0
         });
     }
 
@@ -61,10 +64,13 @@ class App extends Component {
             : "-- °F";
 
         return (
-            <div className="App">
-                <LineChart label={depthLabel} data={depth}/>
-                <LineChart label={temperatureLabel} data={temperature}/>
-                <Compass direction={direction}/>
+            <div className="container">
+                <img alt="Front View" src="http://192.168.0.1:8080/stream/video.mjpeg" width="1102" height="830"/>
+                <div className="overlay">
+                    <LineChart label={depthLabel} data={depth}/>
+                    <LineChart label={temperatureLabel} data={temperature}/>
+                    <Compass direction={direction}/>
+                </div>
             </div>
         );
     }

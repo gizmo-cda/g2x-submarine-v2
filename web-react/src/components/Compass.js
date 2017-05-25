10afea907f6de11468d5e2bc5ec2724e6bfd2e12
Kevin Lindsey <kevin@kevlindev.com>
2017-05-25 15:32:08 -0700

    Try to show our heading as a name

--- web-react/src/components/Compass.js
+++ web-react/src/components/Compass.js
@@ -1,11 +1,32 @@
 import React from 'react';
+import Label from './Label';
+
+let directions = [
+    "N",
+    "NNE",
+    "NE",
+    "ENE",
+    "E",
+    "ESE",
+    "SE",
+    "SSE",
+    "S",
+    "SSW",
+    "SW",
+    "WSW",
+    "W",
+    "WNW",
+    "NW",
+    "NNW"
+];
 
 class Compass extends React.Component {
     static defaultProps = {
         width: 100,
         height: 100,
         color: "rgb(255, 128, 0)",
-        pointer_color: "rgb(200, 200, 200)"
+        pointer_color: "rgb(200, 200, 200)",
+        direction: 0
     };
 
     render() {
@@ -19,16 +40,23 @@ class Compass extends React.Component {
             color       = this.props.color,
             offset      = `translate(${radius - 6 - 1}, 0)`;
 
+        let pointerRotation = `rotate(${this.props.direction - 90})`;
+
+        let quadrantSize = 360 / directions.length;
+        let index = Math.floor((this.props.direction / quadrantSize) + 0.5) % directions.length;
+        let compassText = directions[index];
+
         return (
             <svg className="compass" width={width} height={height}>
                 <g transform={origin}>
                     <circle r={radius} stroke={color} strokeWidth={1} fill="none"/>
-                    <g transform={this.props.direction}>
+                    <g transform={pointerRotation}>
                         <g transform={offset}>
                             <polygon points="6,0 0,4 0,-4" fill={this.props.pointer_color}/>
                         </g>
                     </g>
                 </g>
+                <Label x={half_width} y={half_height + 7} alignment="middle">{compassText}</Label>
             </svg>
         )
     }

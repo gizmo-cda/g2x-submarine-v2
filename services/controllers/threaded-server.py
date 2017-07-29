9a9f6fea82e84169234cccc9443396745d648302
Kevin Lindsey <kevin@kevlindev.com>
2017-07-28 23:09:44 -0700

    Setup a webserver along side the controller server
    This allows us to change thruster settings via a web UI. Right now, “simulation mode” and “calibration mode” are hard-wired to be on. We need to the ability to set these from the command-line.

--- services/controllers/threaded-server.py
+++ services/controllers/threaded-server.py
@@ -11,8 +11,46 @@ from thruster_controller import ThrusterController
 HOST = "0.0.0.0"
 PORT = 9999
 
+# These will eventually be set from the command-line
+SIMULATE = True
+CALIBRATE = True
 
-def on_new_client(clientsocket, addr):
+controller = ThrusterController(SIMULATE)
+
+
+def on_calibration_server(controller):
+    import os
+    from bottle import put, request, route, run, static_file
+
+    script_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), "calibration")
+    css_dir = os.path.join(script_dir, "css")
+    js_dir = os.path.join(script_dir, "js")
+
+    @route('/')
+    def index():
+        return static_file('index.html', root=script_dir)
+
+    @route('/css/<filename>')
+    def serve_css(filename):
+        return static_file(filename, root=css_dir)
+
+    @route('/js/<filename>')
+    def server_js(filename):
+        return static_file(filename, root=js_dir)
+
+    @route('/api/settings')
+    def api_settings():
+        return controller.get_settings()
+
+    @put('/api/settings')
+    def put_settings():
+        controller.set_settings(request.json)
+        return {'status': 'OK'}
+
+    run(host='localhost', port=8080)
+
+
+def on_new_client(controller, clientsocket, addr):
     '''
     This function is responsible for communicating with an active socket
     connection. All input is passed directly to the thruster controller making
@@ -25,8 +63,6 @@ def on_new_client(clientsocket, addr):
     the client are compatible. This implies that our responses need to grow in
     complexity.
     '''
-    controller = ThrusterController()
-
     while True:
         msg = clientsocket.recv(1024)
 
@@ -57,6 +93,9 @@ def close_socket():
     s.close()
 
 
+if CALIBRATE:
+    _thread.start_new_thread(on_calibration_server, (controller,))
+
 # make sure to close our socket when the script exits
 atexit.register(close_socket)
 
@@ -70,7 +109,7 @@ s.bind((HOST, PORT))
 # requests to queue up before we start refusing connections
 s.listen(5)
 
-print("Server bound to {}:{}".format(HOST, PORT))
+print("Thruster server bound to {}:{}".format(HOST, PORT))
 
 while True:
     # wait for an incoming connection
@@ -82,4 +121,4 @@ while True:
     # start a new thread to handle the connection. This prevents this thread
     # from hanging so it can process any other new connections that might
     # come in
-    _thread.start_new_thread(on_new_client, (c, addr))
+    _thread.start_new_thread(on_new_client, (controller, c, addr))

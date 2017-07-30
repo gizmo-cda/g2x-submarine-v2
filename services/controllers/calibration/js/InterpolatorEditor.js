1bfa817fe646995cee51cf0ab16a546ce249d8df
Kevin Lindsey <kevin@kevlindev.com>
2017-07-30 12:40:05 -0700

    First pass at making interpolators editable
    This does not support adding new points to the interpolator

---
+++ services/controllers/calibration/js/InterpolatorEditor.js
@@ -0,0 +1,67 @@
+class InterpolatorEditor {
+    constructor(xMin, xMax, yMin, yMax, bbox) {
+        this.xMin = xMin;
+        this.xMax = xMax;
+        this.yMin = yMin;
+        this.yMax = yMax;
+        this.bbox = bbox;
+
+        this._interpolator = null;
+        this.rootNode;
+        this.currentHandle = null;
+        this.handles = [];
+    }
+
+    set interpolator(value) {
+        this._interpolator = value;
+    }
+
+    get interpolator() {
+        return this._interpolator;
+    }
+
+    attach(node) {
+        this.rootNode = document.createElementNS(svgns, "g");
+
+        if (this._interpolator !== null && this._interpolator !== undefined) {
+            let right = this.bbox.x + this.bbox.width;
+            let bottom = this.bbox.x + this.bbox.height;
+
+            this._interpolator.data.forEach(entry => {
+                let x = map(entry.index, this.xMin, this.xMax, this.bbox.x, right);
+                let y = map(entry.value, this.yMin, this.yMax, bottom, this.bbox.y);
+                let handle = new Handle(this, x, y);
+
+                this.handles.push(handle);
+                handle.attach(this.rootNode);
+            });
+        }
+
+        node.appendChild(this.rootNode);
+    }
+
+    onhandlemove(handle) {
+        var entryIndex = -1;
+
+        for (var i = 0; i < this.handles.length; i++) {
+            if (this.handles[i] === handle) {
+                entryIndex = i;
+                break;
+            }
+        }
+        
+        if (entryIndex != -1) {
+            this._interpolator.data.splice(entryIndex, 1);
+
+            let right = this.bbox.x + this.bbox.width;
+            let bottom = this.bbox.x + this.bbox.height;
+            let index = map(handle.x, this.bbox.x, right, this.xMin, this.xMax);
+            let value = map(handle.y, bottom, this.bbox.y, this.yMin, this.yMax);
+
+            this._interpolator.addIndexValue(index, value);
+
+            // TODO: this should not be called directly
+            iGraph.drawData();
+        }
+    }
+}

5b25eab0f499f2429a091dc33172fe71d1111060
Kevin Lindsey <kevin@kevlindev.com>
2017-08-01 07:03:13 -0700

    Make output more verbose
    This also introduces constants for the supported input types: axis and button

--- services/controllers/thruster_automation.py
+++ services/controllers/thruster_automation.py
@@ -12,17 +12,18 @@ JR_H = 2  # right joystick horizontal axis
 JR_V = 3  # right joystick vertical axis
 
 controller = 0
-type = 0
+AXIS = 0
+BUTTON = 1
 
-host = "192.168.0.1"
-port = 9999
+HOST = "192.168.0.1"
+PORT = 9999
 
 # process command line args
 for i in range(1, len(sys.argv)):
     arg = sys.argv[i]
 
     if arg == "-h" or arg == "--host":
-        host = sys.argv[i + 1]
+        HOST = sys.argv[i + 1]
 
 
 def send_message(controller, type, index, value):
@@ -33,10 +34,14 @@ def send_message(controller, type, index, value):
     m.input_index = index
     m.input_value = value
 
+    if type == AXIS:
+        print("Setting axis {} to {}".format(index, value))
+    elif type == BUTTON:
+        print("Setting button {} to {}".format(index, value))
+
     s.send(bytes(m))
 
     response = s.recv(1024)
-
     decoded_response = response.decode('ascii')
 
     if decoded_response != "OK":
@@ -44,12 +49,12 @@ def send_message(controller, type, index, value):
 
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((host, port))
+s.connect((HOST, PORT))
 print("Connected to server")
 
 # send 50% forward for 1 second
-send_message(controller, type, JL_V, 0.5)
+send_message(controller, AXIS, JL_V, 0.5)
 time.sleep(1)
-send_message(controller, type, JL_V, 0.0)
+send_message(controller, AXIS, JL_V, 0.0)
 
 s.close()

dd56bd67c861c484039c6aede93ab3437aa1c576
Kevin Lindsey <kevin@kevlindev.com>
2017-08-21 18:43:05 -0700

    Remove comment

--- scratchpad/gamepad/lib/ThrusterClient.js
+++ scratchpad/gamepad/lib/ThrusterClient.js
@@ -4,8 +4,6 @@ class ThrusterClient {
     }
 
     sendMessage(controller, type, index, value) {
-        // map axes values to [-1,1]
-
         // build message binary payload per services/controllers/message.py
         let fa = new Float32Array([value]);
         let ba = new Int8Array(fa.buffer);
